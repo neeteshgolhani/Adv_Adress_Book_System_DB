3863007e020b3b0c17188df48bd559121b93902b
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-06-02 01:11:08 +0530

    Added:Ability to insert new Contacts to Address Book

---
+++ CreateNewContact.sql
@@ -0,0 +1,3 @@
+INSERT INTO address_book (first_name, last_name, address, city, state, zip, phone, email) VALUES ('Neetesh', 'Golhani', '123 Main', 'LKD', 'MP', '12345', '+91 9644464646', 'NEE.@example.com');
+ERROR 1050 (42S01): Table 'address_book' already exists
+Query OK, 1 row affected (0.01 sec)
